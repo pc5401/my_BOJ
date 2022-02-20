2f55d8860b382e9be6affc6f27de2682d4f132b0
pc5401 <97648241+pc5401@users.noreply.github.com>
2022-02-21 02:14:14 +0900

    [Bronze I] Title: 수 정렬하기, Time: 176 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/2750.수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750.수 정렬하기/수 정렬하기.py
@@ -1,16 +1,19 @@
-#  bubble sort
+#  selction sort
 n = int(input())
 lst = []
 for _ in range(n):
     num = int(input())
     lst.append(num)
 
-for i in range(n):
-    for j in range(n-1):
-        if lst[j] > lst[j+1]:
-            x = lst[j]
-            lst[j] = lst[j+1]
-            lst[j+1] = x
+for i in range(n-1):  # 최소값을 찾고, 배치한 자리 제외하고 진행
+    min_idx = i  # 최소값 저장소
+    for j in range(i+1, n):
+        if lst[min_idx] > lst[j]:
+            min_idx = j
+        else:
+            pass
+    lst[i], lst[min_idx] = lst[min_idx], lst[i]
+
 
 for _ in range(n):
     print(lst[_])
