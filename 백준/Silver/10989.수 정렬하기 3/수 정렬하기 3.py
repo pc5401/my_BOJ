b8cda3e10695a5cdd164ce0d59a3082f5729511b
pc5401 <97648241+pc5401@users.noreply.github.com>
2022-02-21 03:05:16 +0900

    [Silver V] Title: 수 정렬하기 3, Time: 9852 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/10989.수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,16 @@
+#  selction sort, 
+import sys
+
+n = int(sys.stdin.readline())
+lst = [0 for i in range(10001)]
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    lst[num] += 1
+
+
+for i in range(len(lst)):
+    if lst[i] == 0:
+        pass
+    else:
+        for j in range(lst[i]):
+            print(i)
