dee81d860f3f5c7308a6dd5f49c36c9e57f8f2d4
pc5401 <97648241+pc5401@users.noreply.github.com>
2022-08-25 20:56:33 +0900

    [Silver I] Title: 숨바꼭질, Time: 172 ms, Memory: 34784 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+N, K = map(int, input().split())  # 0~100,000
+
+visit = [0] * 100001
+visit[N] = 1
+que = deque()
+que.append(N)
+
+while que:
+    v = que.popleft()
+
+    if v*2 < 100001 and visit[v*2] == 0:
+        visit[v*2] = visit[v] + 1
+        que.append(v*2)
+        if v*2 == K:
+            break
+
+    if v+1 < 100001 and visit[v + 1] == 0:
+        visit[v+1] = visit[v] + 1
+        que.append(v+1)
+        if v+1 == K:
+            break
+    
+    if 0 <= v-1 and visit[v - 1] == 0:
+        visit[v-1] = visit[v] + 1
+        que.append(v-1)
+        if v-1 == K:
+            break
+    
+    if visit[K]:
+        break
+
+res = visit[K]
+print(res-1)
