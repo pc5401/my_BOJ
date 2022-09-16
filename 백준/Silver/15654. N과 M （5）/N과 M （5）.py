3ba8c66b357d6cdadf2ba3873012d8f63563e96f
pc5401 <97648241+pc5401@users.noreply.github.com>
2022-09-17 08:35:55 +0900

    [Silver III] Title: N과 M (5), Time: 248 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+input_lst = list(map(int , input().split()))
+arr = sorted(input_lst)
+
+def back(lst:list):
+    global N, M
+
+    l = len(lst)
+    if l == M:
+        print(*lst)
+        return
+
+    for i in arr:
+        if i in lst:
+            continue
+        lst.append(i)
+        back(lst)
+        lst.pop()
+
+back([])
