348a06cdbad930c63a21575a4e027b633a55ccf3
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-05-23 17:34:12 +0900

    [Silver III] Title: 01타일, Time: 196 ms, Memory: 69788 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def solve(n:int) -> int:
+    dp = [0] * (n+1)
+    dp[1] = 1
+    if n > 1:
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = (dp[i-1] + dp[i-2]) % 15746
+
+    return dp[n]
+
+
+if __name__ == "__main__":
+    N = int(input())
+    print(solve(N))
