ef427e7a66a4e92643c464cafc54eb9fd71086a6
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-05-24 18:22:21 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -0,0 +1,19 @@
+def dfs(num:int, visit:list, computers:list):
+    visit[num] = 1
+    
+    for i,v in enumerate(computers[num]):
+        if v and not visit[i]:
+            dfs(i,visit,computers)
+            
+
+
+
+def solution(n, computers):
+    answer = 0
+    visit = [0] * n
+    for i in range(n):
+        if visit[i]:
+            continue
+        dfs(i,visit,computers)
+        answer += 1
+    return answer
