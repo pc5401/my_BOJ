3a54e458092e28d9260b554ee675c3751e7d3855
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-06-22 16:20:52 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,16 +1,7 @@
 def solution(citations):
-    answer = 0
-    ans = []
-    n = len(citations)
     citations.sort()
-    for h in range(n):
-        cnt = 0
-        for c in citations:
-            if c > h:
-                break
-            cnt += 1
-            
-        if cnt >= h and h >= n - cnt:
-            ans = h
-    
-    return ans
+    n = len(citations)
+    for i in range(n):
+        if citations[i] >= n - i:
+            return n - i
+    return 0
