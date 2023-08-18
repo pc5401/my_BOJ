3770b00fd0207b7e50b5e110a75da166bd7c7693
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-08-18 16:11:44 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 9500 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -3,16 +3,12 @@ function solve(input) {
   // TODO: 알고리즘 로직
   const N = Number(input[0].trim());
   for (let i = 1; i <= N; i++){
-    res = ''
-    for (let j = 1; j <= i; j++){
-      res += '*'
-    }
-    console.log(res)
+    console.log('*'.repeat(i))
   }
 }
 
-
-const isLocal = true;  // 여기서 true로 설정하면 로컬 환경으로 간주
+// 제출하기전에 false 로
+const isLocal = false;  // 여기서 true로 설정하면 로컬 환경으로 간주
 
 if (isLocal) {
     // 로컬 환경 (VSCode)에서의 입력 처리
