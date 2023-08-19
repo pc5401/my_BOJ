5577716435781fa94e8b75e34b88ddc60bcd8b0e
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-08-19 16:03:21 +0900

    [Bronze II] Title: 이진수 덧셈, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.js
@@ -0,0 +1,33 @@
+// 알고리즘 로직을 처리하는 함수
+function solve(input) {
+  // TODO: 알고리즘 로직
+    const [a, b] = input[0].split(' ').map(num => BigInt(`0b${num}`));
+    const sumValue = a + b;
+
+    let result = sumValue.toString(2);
+    console.log(result);
+}
+
+// 제출하기전에 false 로
+const isLocal = false;  // 여기서 true로 설정하면 로컬 환경으로 간주
+
+if (isLocal) {
+    // 로컬 환경 (VSCode)에서의 입력 처리
+    const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    let lines = [];
+    rl.on('line', (line) => {
+        lines.push(line);
+    }).on('close', () => {
+        solve(lines);
+    });
+} else {
+    // 백준 환경에서의 입력 처리
+    const fs = require('fs');
+    const lines = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+    solve(lines);
+}
