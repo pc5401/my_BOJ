230871b99503cfb1ad3f52b94044f40845fc72bc
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-09-01 14:37:09 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 212 ms, Memory: 10812 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
@@ -1,41 +1,36 @@
 function solve(input) {
-  // TODO: 알고리즘 로직
     const [N, M] = input[0].split(' ').map(Number);
-    // 테이블 생성
     const Arr = new Map();
+    const visit = Array(N+1);
+    let res = 0; 
+    let minV = Infinity;
+
     input.slice(1).forEach(element => {
         const [x, y] = element.split(' ').map(Number);
-        
-        if (Arr.has(x)) {
-            Arr.get(x).push(y);
-        } else {
-            Arr.set(x, [y]);
-        }
-
-        if (Arr.has(y)) {
-            Arr.get(y).push(x);
-        } else {
-            Arr.set(y, [x]);
-        }
+        if (Arr.has(x)) Arr.get(x).push(y);
+        else Arr.set(x, [y]);
+        if (Arr.has(y)) Arr.get(y).push(x);
+        else Arr.set(y, [x]);
     });
 
-    let res = 0; // 결과
-    let minV = Infinity; // 최소값
-
     const bfs = (n) => {
+        for(let i = 0; i <= N; i++) visit[i] = 0;
         const Q = [n];
-        const visit = Array(N+1).fill(0);
+        let sum = 0;
 
         while (Q.length > 0) {
             const v = Q.shift();
-            for (const node of Arr.get(v) || []) {
-                if (visit[node]) continue;
-                Q.push(node);
-                visit[node] = visit[v] + 1;
+            const neighbors = Arr.get(v);
+            if (neighbors) {
+                for (const node of neighbors) {
+                    if (visit[node]) continue;
+                    Q.push(node);
+                    visit[node] = visit[v] + 1;
+                    sum += visit[node];
+                }
             }
         }
-
-        return visit.reduce((a, b) => a + b, 0);
+        return sum;
     }
 
     for (let i = 1; i <= N; i++) {
@@ -47,7 +42,6 @@ function solve(input) {
     }
     console.log(res);
 }
-
 
 // 제출하기전에 false 로
 const isLocal = true;  // 여기서 true로 설정하면 로컬 환경으로 간주
