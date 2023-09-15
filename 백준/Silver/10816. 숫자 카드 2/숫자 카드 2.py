f7ccb5ed9bd53acdee1491be3c26e4ed9caca55a
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-09-15 17:55:25 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1568 ms, Memory: 115856 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,18 @@
+import sys
+import bisect
+input = sys.stdin.readline
+if __name__ == "__main__":
+    N = int(input())
+    my_cards = sorted(list(map(int,input().split())))
+    M = int(input())
+    look_for = list(map(int, input().split()))
+    res_list = [0] * M
+
+    for idx, card in enumerate(look_for):
+        lo = bisect.bisect_left(my_cards, card)
+        if lo == -1:
+            continue
+        hi = bisect.bisect_right(my_cards, card)
+        res_list[idx] = hi - lo
+
+    print(*res_list)
