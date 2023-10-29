5a498d2d7e7b673bd5430fd62b34d984c04721bf
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-10-29 18:52:49 +0900

    [Bronze III] Title: 저작권, Time: 116 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.js
@@ -0,0 +1,29 @@
+function solve(input) {
+    // TODO: 알고리즘 로직
+    const [A, I] = input[0].split(' ').map(Number);
+    console.log(A * I - (A - 1));
+}
+
+// 제출하기전에 false 로
+const isLocal = false;  // 여기서 true로 설정하면 로컬 환경으로 간주
+
+if (isLocal) {
+    // 로컬 환경 (VSCode)에서의 입력 처리
+    const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    let lines = [];
+    rl.on('line', (line) => {
+        lines.push(line);
+    }).on('close', () => {
+        solve(lines);
+    });
+} else {
+    // 백준 환경에서의 입력 처리
+    const fs = require('fs');
+    const lines = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+    solve(lines);
+}
