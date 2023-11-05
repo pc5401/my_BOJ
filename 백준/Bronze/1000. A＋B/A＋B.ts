8fbf58dbbe1ba000b3278b6408e7fade1b45cce1
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-11-05 19:03:00 +0900

    [Bronze V] Title: A+B, Time: 160 ms, Memory: 9484 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.ts
@@ -0,0 +1,12 @@
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line',(line: string) => {
+    const [A, B] = line.split(' ').map(Number);
+    console.log(A+B);
+    rl.close;
+})
