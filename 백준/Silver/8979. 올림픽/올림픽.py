4bfbba478b7e875498b26919d3cfe6b8e82283ec
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-11-11 16:52:20 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 31120 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == "__main__":
+    N, K = map(int, input().split())
+    lst = [list(map(int, input().split())) for _ in range(N)]
+    lst.sort(key=lambda x: (-x[1], -x[2], -x[3]))
+    ranks = [1] * N
+    k_rank = 0
+    for i in range(1, N):
+        if lst[i][1] == lst[i-1][1] and lst[i][2] == lst[i-1][2] and lst[i][3] == lst[i-1][3]:
+            ranks[i] = ranks[i-1]
+        else:
+            ranks[i] = ranks[i-1] + 1
+        
+        if lst[i][0] == K:
+            k_rank = ranks[i]
+            break
+
+    print(k_rank)
+
