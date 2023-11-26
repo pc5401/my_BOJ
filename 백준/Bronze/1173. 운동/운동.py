ccd9a69cb3788566b497649bf4cd419347138601
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-11-26 20:22:38 +0900

    [Bronze II] Title: 운동, Time: 60 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/1173. 운동/운동.py
+++ 백준/Bronze/1173. 운동/운동.py
@@ -6,15 +6,16 @@ if __name__ == "__main__":
     N, m, M, T, R  = map(int, input().split())
     total, time, X = 0, 0, m
 
-    while time < N:
-        if (X+T) <= M: # 운동
-            X += T
-            time += 1
-        else:
-            X = max(m, X-R)
+    if m + T > M:
+        print(-1)
+    else:
+        while time < N:
+            if (X+T) <= M: # 운동
+                X += T
+                time += 1
+            else:
+                X = max(m, X-R)
+            
+            total += 1
         
-        total += 1
-        if total > 1e6:
-            break
-    
-    print(total if total < 1e6 else -1)
+        print(total)
