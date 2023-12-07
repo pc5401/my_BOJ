ec6dd64c2abe3d49be8e0668ee88056170d0bf76
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-12-07 15:56:08 +0900

    [Bronze V] Title: 문자와 문자열, Time: 152 ms, Memory: 9472 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,30 @@
+function solve(input) {
+    // TODO: 알고리즘 로직
+    const S = input[0]
+    const I = Number(input[1])
+    console.log(S.at(I-1))
+}
+
+// 제출하기전에 false 로
+const isLocal = true;  // 여기서 true로 설정하면 로컬 환경으로 간주
+
+if (isLocal) {
+    // 로컬 환경 (VSCode)에서의 입력 처리
+    const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    let lines = [];
+    rl.on('line', (line) => {
+        lines.push(line);
+    }).on('close', () => {
+        solve(lines);
+    });
+} else {
+    // 백준 환경에서의 입력 처리
+    const fs = require('fs');
+    const lines = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+    solve(lines);
+}
