6598dcc0d4b51e013e32d4df318b09f94b8f4bb7
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-12-09 17:31:51 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 336 ms, Memory: 55652 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -5,25 +5,21 @@ input = sys.stdin.readline
 def find(node):
     if union_find[node] != node:
         union_find[node] = find(union_find[node])
-    
-    return union_find[node] # 재귀
-
-
-def union(a, b):
-    A, B = find(a), find(b)
-    if A != B:
-        union_find[A] = B
+    return union_find[node]
 
 
 if __name__ == "__main__":
-    V, E = map(int, input().split())
-    lst = [list(map(int,input().split())) for _ in range(E)]
-    lst.sort(key=lambda x: x[2])
+    N, M = map(int, input().split())
+    bills = [list(map(int, input().split())) for _ in range(M)]
+    union_find = {i:i for i in range(1, N+1)}
+    bills.sort(key=lambda x: x[2])
     res = 0
-    union_find = { i:i for i in range(1, V+1)}
 
-    for a, b, c in lst:
-        if find(a) != find(b):
-            union(a, b)
-            res += c
+    for a, b, c in bills:
+        A, B = find(a), find(b)
+        if A == B: # 연결됨
+            continue
+        union_find[A] = B
+        res += c
+
     print(res)
