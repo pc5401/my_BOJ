9aae0da9418875f3b4784067abf0b5ff01039ffd
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-12-10 18:47:22 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 224 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,37 +1,29 @@
 import sys
-import collections
 input = sys.stdin.readline
 
-def solve(n:int) -> int:
-    lst = [0] * (N+1)
-    Q = collections.deque()
-    Q.append(n)
-
-    while Q:
-        v = Q.popleft()
-        for i in data[v]:
-            if lst[i]:
-                continue
-            lst[i] = lst[v] + 1
-            Q.append(i)
-    return sum(lst)
 
 if __name__ == "__main__":
-    # input 값
-    N, M = map(int,input().split())
-    data = collections.defaultdict(list)
-    for m in range(M):
-        a, b = map(int,input().split())
-        data[a].append(b)
-        data[b].append(a)
-    # 세팅 값
-    res = 0
-    minV = 1e9
-    # 풀이 시작
-    for num in range(1,N):
-        value = solve(num)
-        if value < minV:
-            minV = value
-            res = num
+    N, M = map(int, input().split())
+    network = [[1e9]*N for _ in range(N)]
+    for _ in range(M):
+        A, B = map(int, input().split())
+        network[A-1][B-1] = 1
+        network[B-1][A-1] = 1
 
-    print(res)
+    for k in range(N):
+        network[k][k] = 0
+        for i in range(N):
+            for j in range(N):
+                if network[i][j] > network[i][k] + network[k][j]:
+                    network[i][j] = network[i][k] + network[k][j]
+
+    res = 0
+    sumV = 1e9
+    for i in range(N):
+        value = sum(network[i])
+        if sumV > value:
+            res = i
+            sumV = value
+    
+    print(res+1)
+
