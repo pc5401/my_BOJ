d45bc3c3c05b8fb5d533d3d0c52c857055855fac
pc5401 <97648241+pc5401@users.noreply.github.com>
2023-12-11 17:32:21 +0900

    [Silver IV] Title: 수들의 합 2, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    stacked_A = [0]
+    for i in range(N):
+        stacked_A.append(stacked_A[-1] + A[i])
+    
+
+    # 투 포인터
+    lo, hi = 0, 1
+    res = 0
+    while lo <= hi and hi <= N:
+        sumV = stacked_A[hi] - stacked_A[lo]
+        if sumV < M:
+            hi += 1
+        elif sumV > M:
+            lo += 1
+        else:
+            lo += 1
+            res += 1
+
+    print(res)
