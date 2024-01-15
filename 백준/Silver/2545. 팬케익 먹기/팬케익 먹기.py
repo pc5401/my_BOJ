1c214a03fc2ef1e360327178b6232ba679805588
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-01-15 16:06:12 +0900

    [Silver V] Title: 팬케익 먹기, Time: 44 ms, Memory: 31120 KB, Score: 3 point -BaekjoonHub

--- 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.py
+++ 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.py
@@ -1,25 +1,51 @@
 import sys
-import decimal
 input = sys.stdin.readline
 
+def solve(cake: tuple) -> int:
+    a, b, c, D = cake
 
-def solve(abc:list, d: int) -> int:
+    if D > 0:
+        cut = [a, b, c]
+        cut.sort() # 크기 순으로 a, b, c
+        a, b, c = cut
+        x = c - b
+        if x >= D:
+            return (c-D) * b * a
+        D -= x
+        c -= x
+        # b, c 동일
+        x = b - a
+        if 2*x >= D:
+            p, q = D//2, D%2
+            return (c-p-q)*(b-p)*a
+        c-= x
+        b -= x
+        D -= (2*x)
+        # a, b, c 동일
+        p, q = D//3, D%3
+        a -= p
+        b -= p 
+        c -= p
+        if q == 2:
+            return (c-1)*(b-1)*a
+        elif q == 1:
+            return (c-1)*b*a
     
-    while d >0 and abc[2] > 1:
-        abc[2] -= 1
-        d -= 1
-        abc.sort()
+    return a*b*c
 
-    return abc[0] * abc[1] * abc[2]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     T = int(input())
+    lst = []
+    for _ in range(T):
+        input()
+        lst.append(tuple(map(int,input().split())))
+    
     res = []
-    for tc in range(T):
-        null_input = input()
-        lst = list(map(decimal.Decimal, input().split()))
-        D = lst.pop()
-        lst.sort()
-        res.append(solve(lst, D))
+    
+    for i in lst:
+        res.append(solve(i))
+    
     for r in res:
         print(r)
