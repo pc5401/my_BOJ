e0cef8dcfe80948a31c4e177d2c689f39d5b3a11
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-01-22 20:02:08 +0900

    [Silver II] Title: 병사 배치하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -1,14 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-
-if __name__ == "__main__": # LIS DP 접근
+if __name__ == "__main__": # LIS 이분탐색
     N = int(input())
-    soldiers = list(map(int, input().split()))
-    dp = [1] * N
-    for i in range(1, N):
-        for j in range(0, i):
-            if soldiers[i] < soldiers[j] and dp[i] < dp[j] + 1:
-                dp[i] = dp[j] + 1
-    
-    print(N - max(dp))
+    lst = list(map(int,input().split()))
+    LIS = []
+    for i in range(N):
+        if LIS and LIS[-1] > lst[i]:
+            LIS.append(lst[i])
+        else: # 이분탐색
+            lo, hi = 0, len(LIS)
+
+            while lo < hi:
+                mid = (lo+hi) // 2
+                if lst[i] >= LIS[mid]:
+                    hi = mid
+                else:
+                    lo = mid + 1
+            
+            if lo == len(LIS):
+                LIS.append(lst[i])
+            else:
+                LIS[lo] = lst[i]
+
+    print(N - len(LIS))
