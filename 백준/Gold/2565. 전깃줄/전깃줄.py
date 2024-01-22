d5232f808ed01726e9df8caf4c8ec9b8405fae28
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-01-22 20:37:22 +0900

    [Gold V] Title: 전깃줄, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -1,13 +1,25 @@
-if __name__ == '__main__':
-    AB = []
+if __name__ == "__main__": # LIS 이분탐색
     N = int(input())
+    line = [tuple(map(int,input().split())) for _ in range(N)]
+    line.sort(key=lambda x: x[0])
+    LIS = []
+
     for i in range(N):
-        AB.append(list(map(int,input().split())))
-    AB.sort(key=lambda x : x[0])
-    dp = [1] * N
-    for i in range(1,N):
-        for j in range(0,i):
-            if AB[i][1] > AB[j][1] and dp[i] < dp[j] + 1:
-                dp[i] = dp[j] + 1
-    res = N -max(dp)
-    print(res)
+        if LIS and LIS[-1] < line[i][1]:
+            LIS.append(line[i][1])
+        else: # 이분탐색
+            lo, hi = 0, len(LIS)
+
+            while lo < hi:
+                mid = (lo+hi) // 2
+                if line[i][1] > LIS[mid]:
+                    lo = mid + 1
+                else:
+                    hi = mid
+            
+            if lo == len(LIS):
+                LIS.append(line[i][1])
+            else:
+                LIS[lo] = line[i][1]
+
+    print(N - len(LIS))
