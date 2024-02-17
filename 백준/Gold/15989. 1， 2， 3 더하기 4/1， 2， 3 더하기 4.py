ff49bbbb0db50ee5b8860cc42dcee6de0859f648
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-02-17 17:45:00 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -5,13 +5,11 @@ input = sys.stdin.readline
 if __name__ == '__main__':
     T = int(input())
     n_lst = [int(input()) for _ in range(T)]
-    dp = [1]*10001
+    dp = [0]*10001
+    dp[1], dp[2], dp[3] =1, 2, 3
 
-    for n in range(2, 10001):
-        dp[n] += dp[n-2]
+    for n in range(10001):
+        dp[n] = dp[n-3] + int(n/2) + 1
     
-    for n in range(3, 10001):
-        dp[n] += dp[n-3]
-
     for n in n_lst:
         print(dp[n])
