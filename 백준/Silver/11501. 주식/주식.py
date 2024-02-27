ddbb6434cadcc6a75c3738e35824006889a91c07
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-02-27 15:47:05 +0900

    [Silver II] Title: 주식, Time: 2344 ms, Memory: 176204 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -3,20 +3,14 @@ input = sys.stdin.readline
 
 def find_maxmum_profit(n: int, prices: list)-> int:
     total_profit = 0
-    day_max_price = [(n-1, prices[-1])]
+    max_price = prices[-1]
 
     for day in range(n-2, -1, -1):
         price = prices[day]
-        if price > day_max_price[-1][1]:
-            day_max_price.append((day, price))
-    
-    start = 0
-    while day_max_price:
-        day, max_price = day_max_price.pop()
-        for i in range(start, day):
-            total_profit += (max_price - prices[i])
-
-        start = day+1
+        if price > max_price:
+            max_price = price
+            continue
+        total_profit += (max_price -price)
 
     return total_profit
 
