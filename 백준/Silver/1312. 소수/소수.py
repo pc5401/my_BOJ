a2d3d4012f793a4e9b163e27671a9771327d9807
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-03-18 17:32:11 +0900

    [Silver V] Title: 소수, Time: 148 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1312. 소수/소수.py
+++ 백준/Silver/1312. 소수/소수.py
@@ -4,11 +4,19 @@ input = sys.stdin.readline
 
 def main():
     A, B, N = map(int,input().split())
-    A = A * (10**N)
-    value = A // B
-    print(value % 10)
+    
+    x = A % B
+    res = 0
+    for _ in range(N):
+        x *= 10
+        if x // B:
+            res = x // B
+            x = x % B
+        else:
+            res = 0
+    print(res)
+
 
 
 if __name__ == "__main__":
     main()
-
