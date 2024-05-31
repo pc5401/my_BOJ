087562839c774694875ba5a6bd59a4645b2634ba
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-05-31 21:29:50 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 1948 ms, Memory: 124616 KB -BaekjoonHub

---
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+
+def solve(n: int, k: int) -> int:
+    if n == 1 or n == 2:
+        return 1
+    elif k == 1:
+        return 1
+    elif n == k:
+        return 1
+
+    return solve(n-1, k-1) + solve(n-1, k)
+
+def main():
+    # 입력값
+    n, k = map(int, input().split())
+    # 풀이
+    result: int = solve(n, k)
+    # 출력
+    
+    print(result)
+    
+if __name__ == "__main__":
+    main()
+
