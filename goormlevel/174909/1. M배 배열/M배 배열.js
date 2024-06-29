51f8a7e7455c08a531d2cc67097d9d4d33814965
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-06-29 18:16:40 +0900

    [goormlevel] Title: M배 배열, Time: 0.05 ms, Memory: 36.49 MB, Difficulty: 2 -BaekjoonHub

--- goormlevel/174909/1. M배 배열/M배 배열.js
+++ goormlevel/174909/1. M배 배열/M배 배열.js
@@ -1,25 +1,23 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let input = [];
-rl.on('line', (line) => {
-    input.push(line);
-    if (input.length === 2) {
-        rl.close();
-    }
-});
-
-rl.on('close', () => {
-    const [N, M] = input[0].split(' ').map(Number);
-    const A = input[1].split(' ').map(Number);
-
-    console.log(solve(N, M, A));
-});
-
 const solve = (N, M, A) => {
     return A.map(e => e % M === 0 ? e : e * M).join(' ');
 }
+
+// Run by Node.js
+(async () => {
+    const rl = readline.createInterface({ input: process.stdin });
+    const input = [];
+
+    for await (const line of rl) {
+        input.push(line);
+        if (input.length === 2) {
+            const [N, M] = input[0].split(' ').map(Number);
+            const A = input[1].split(' ').map(Number);
+            const result = solve(N, M, A);
+            console.log(result);
+            input.length = 0; // Clear the input array for the next case
+        }
+    }
+    rl.close();
+})();
