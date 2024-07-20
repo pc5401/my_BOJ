a2b54d7a3d952528b4e22dfd1cd0127a2f9115b1
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-07-20 23:54:41 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,14 @@
+def main():
+    w = input().strip()
+    
+    if w == "SONGDO":
+        print("HIGHSCHOOL")
+    elif w == "CODE":
+        print("MASTER")
+    elif w == "2023":
+        print("0611")
+    elif w == "ALGORITHM":
+        print("CONTEST")
+
+if __name__ == "__main__":
+    main()
