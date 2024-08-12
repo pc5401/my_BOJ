afb9508fe56e3fa89425acce74f9e6a52621d0ed
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-08-12 17:28:16 +0900

    [Silver V] Title: 피자 (Small), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14606. 피자 （Small）/피자 （Small）.py
+++ 백준/Silver/14606. 피자 （Small）/피자 （Small）.py
@@ -1,14 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-
-def solve(N: int):
-    dp = [0, 0, 1, 3, 6, 10, 15, 21, 28, 36, 45]
-    return dp[N]
-
-
 def main():
     N = int(input())
-    print(solve(N))
+    dp = [0, 0, 1, 3, 6, 10, 15, 21, 28, 36, 45]
+    print(dp[N])
 
 main()
+
