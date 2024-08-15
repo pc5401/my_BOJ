96e8a8ad56ca770fb63c8b48b3b08783d33fe3d7
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-08-15 16:15:21 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 44 ms, Memory: 41064 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+def solve(n: int, memo: dict[int, str]):
+    if not n in memo:
+        memo[n] = solve(n//3, memo)
+    else:
+        return memo[n]
+
+    w = memo[n].split('\n')
+    rtn = []
+    m = n // 3
+    for i in range(m):
+        rtn.append(w[i] * 3)
+
+    for i in range(m):
+        rtn.append(w[i] + (' '*m) + w[i]) 
+
+    for i in range(m):
+        rtn.append(w[i] * 3)
+
+    return '\n'.join(rtn)
+
+def main():
+    N = int(input())
+    memo = {1 :'*'}
+    print(solve(N, memo))
+
+main()
