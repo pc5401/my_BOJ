6561b64c1ef5d2b0f4f90ab842123b094afbe0ed
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-08-16 23:24:25 +0900

    [Gold V] Title: 두 개의 탑, Time: 200 ms, Memory: 16400 KB -BaekjoonHub

---
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.js
@@ -0,0 +1,42 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input : process.stdin,
+  output : process.output
+})
+
+let input = [];
+
+rl.on('line', (line)=>{
+  input.push(Number(line));
+  if (input[0] < input.length) rl.close();
+})
+
+rl.on('close', ()=>{
+  const N = input[0];
+  const arr = input.slice(1);
+  const result = solve(N, arr);
+  console.log(result);
+})
+
+
+const solve = (n, arr) => {
+  let rtn = 0;
+  for (let i = 1; i<n; i++) arr[i] += arr[i-1]; // 누적합
+  arr.unshift(0)
+  let lo = 0;
+  let hi = 1;
+
+  while (lo < hi){
+    clockwise = arr[hi] - arr[lo];
+    counterclockwise = (arr[n] - arr[hi]) + arr[lo];
+    rtn = Math.max(rtn, Math.min(clockwise, counterclockwise));
+
+    if (counterclockwise > clockwise) hi++;
+    else lo++;
+
+    if (hi >= n) break;
+  }
+
+  return rtn;
+}
