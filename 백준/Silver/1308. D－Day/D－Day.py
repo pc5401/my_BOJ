3cb179443d3ba53fd5b574e33fec040f36161d7e
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-09-11 17:52:08 +0900

    [Silver V] Title: D-Day, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1308. D－Day/D－Day.py
+++ 백준/Silver/1308. D－Day/D－Day.py
@@ -1,13 +1,40 @@
 import sys
-import datetime
 input = sys.stdin.readline
 
+def count_days(date: list[int]) -> int:
+    y, m, d = date
+    months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-if __name__ == '__main__':
-    # 입력값
+    cnt = y * 365 + sum([months[i] for i in range(m)]) + d
+
+    cnt += ((y-1) // 4)
+    cnt -= ((y-1) // 100)
+    cnt += ((y-1) // 400)
+
+    if y % 4 == 0 and (not y % 100 == 0 or  y % 400 == 0):
+        if m > 2:
+            cnt += 1
+
+    return cnt
+
+
+def solve(t: list[int], d: list[int]) -> int:
+    if d[0] - t[0] > 1000:
+        return 'gg'
+
+    if d[0] - t[0] == 1000:
+        if d[1] > t[1]:
+            return 'gg'
+        elif d[1] == t[1] and d[2] >= t[2]:
+            return 'gg'
+        
+    return f'D-{count_days(d) - count_days(t)}'
+
+if __name__ == "__main__":
     today = list(map(int, input().split()))
     d_day = list(map(int, input().split()))
 
-    remaining =  datetime.datetime(d_day[0], d_day[1], d_day[2]) - datetime.datetime(today[0], today[1], today[2])
 
-    print(f'D-{remaining.days}' if datetime.datetime(today[0]+1000, today[1], today[2]) > datetime.datetime(d_day[0], d_day[1], d_day[2]) else 'gg')
+    result = solve(today, d_day)
+
+    print(result)
