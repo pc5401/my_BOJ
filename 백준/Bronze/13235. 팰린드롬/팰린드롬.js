24668cab472457bc697a31b86691987ab134470c
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-10-12 23:38:26 +0900

    [Bronze II] Title: 팰린드롬, Time: 104 ms, Memory: 9460 KB -BaekjoonHub

--- 백준/Bronze/13235. 팰린드롬/팰린드롬.js
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.js
@@ -6,16 +6,7 @@ const rl = readline.createInterface({
 });
 
 rl.on('line', (input) => {
-  const len = input.length;
-  let isPalindrome = true;
-  
-  for (let i = 0; i < Math.floor(len / 2); i++) {
-    if (input[i] !== input[len - 1 - i]) {
-      isPalindrome = false;
-      break;
-    }
-  }
-  
+  const isPalindrome = input === input.split('').reverse().join('');
   console.log(isPalindrome ? "true" : "false");
   rl.close();
 });
