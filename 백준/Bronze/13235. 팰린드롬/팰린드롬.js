3296b2746b6c45da9ea27dddf5e4a2ff286a7b92
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-10-12 23:38:04 +0900

    [Bronze II] Title: 팰린드롬, Time: 104 ms, Memory: 9460 KB -BaekjoonHub

--- 백준/Bronze/13235. 팰린드롬/팰린드롬.js
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.js
@@ -1,12 +1,21 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  input: process.stdin,
+  output: process.stdout
+});
 
-rl.on('line', a => {
-    const b = [...a].reduce((a,b)=>b+a, '');
-    console.log(a===b)
-    rl.close()
+rl.on('line', (input) => {
+  const len = input.length;
+  let isPalindrome = true;
+  
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (input[i] !== input[len - 1 - i]) {
+      isPalindrome = false;
+      break;
+    }
+  }
+  
+  console.log(isPalindrome ? "true" : "false");
+  rl.close();
 });
