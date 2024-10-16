4265b1fed62444dc409c10851412b5796e656006
pc5401 <97648241+pc5401@users.noreply.github.com>
2024-10-16 18:52:05 +0900

    [Bronze II] Title: 피자는 나눌 수록 커지잖아요, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/17946. 피자는 나눌 수록 커지잖아요/피자는 나눌 수록 커지잖아요.py
+++ 백준/Bronze/17946. 피자는 나눌 수록 커지잖아요/피자는 나눌 수록 커지잖아요.py
@@ -2,19 +2,11 @@ import sys
 input = sys.stdin.readline      
 
 
-def solve(n: int) -> int:
-    return 1
-
-     
-
 if __name__ == "__main__":
     # 입력값
     N = int(input())
     lst = [int(input()) for _ in range(N)]
-    
-    # 풀이
-    result = [solve(n) for n in lst]
 
     # 출력
-    for res in result:
-        print(res)
+    for _ in range(N):
+        print(1)
