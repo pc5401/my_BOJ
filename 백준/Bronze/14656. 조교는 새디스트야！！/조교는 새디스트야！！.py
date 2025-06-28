4fbc8b4eba6d167e058a3611066a134dba23057d
pc5401 <97648241+pc5401@users.noreply.github.com>
2025-06-28 12:11:09 +0900

    [Bronze III] Title: 조교는 새디스트야!!, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/14656. 조교는 새디스트야！！/조교는 새디스트야！！.py
+++ 백준/Bronze/14656. 조교는 새디스트야！！/조교는 새디스트야！！.py
@@ -1,30 +1,5 @@
 import sys
-import collections
-input = sys.stdin.readline
-
-
-def solve(N: int, nums: list[int]) -> int:
-    rtn, i = 0, 0
-
-    while i < N:
-        num = nums[i]
-        i += 1
-
-        if num != i:
-            rtn += 1
-
-    return rtn
-
-
-def main():
-    # 입력
-    N = int(input())
-    nums = list(map(int, input().split()))
-    # 풀이
-    result = solve(N, nums)
-
-    # 출력
-    print(result)
-
-if __name__ == "__main__":
-    main()
+tokens = sys.stdin.read().split()
+nums = map(int, tokens[1:])
+# 맞지 않는 경우를 1로 세어서 합산
+print(sum(1 for i, v in enumerate(nums, 1) if i != v))
